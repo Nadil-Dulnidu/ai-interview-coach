ec1e68393562215da736988a4c7b98e5a3878854
Nadil-Dulnidu <nzinlud963@gmail.com>
2026-08-21 15:27:43 +0530

    fix: add missing CORS origin for localhost:5174

--- server/app/main.py
+++ server/app/main.py
@@ -17,6 +17,7 @@ app.add_middleware(
         "http://localhost:5173",  # Vite dev server
         "http://localhost:3000",  # Alternative dev port
         "http://127.0.0.1:5173",
+        "http://localhost:5174",
         "http://127.0.0.1:3000",
     ],
     allow_credentials=True,
